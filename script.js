ebad074133298d2b2cca757ec66fdbc6ed27b29c
Brock Davis <brockdavis@outlook.com>
2023-03-16 16:33:59 -0400

    Add corner hits counter

--- script.js
+++ script.js
@@ -17,40 +17,45 @@ document.addEventListener('DOMContentLoaded', () => {
   let xSpeed = 2;
   let ySpeed = 2;
   let tintColor = randomColor();
+  let cornerHits = 0;
 
   function updateDimensions() {
-  const viewport = window.visualViewport;
-  canvas.width = viewport.width;
-  canvas.height = viewport.height;
-  logoWidth = canvas.width * 0.25;
-  logoHeight = (logoWidth * img.height) / img.width;
+    const viewport = window.visualViewport;
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+    logoWidth = canvas.width * 0.25;
+    logoHeight = (logoWidth * img.height) / img.width;
 
-  // Check if the current x and y positions place the logo outside the canvas bounds
-  if (x + logoWidth > canvas.width) {
-    x = canvas.width - logoWidth;
+    // Check if the current x and y positions place the logo outside the canvas bounds
+    if (x + logoWidth > canvas.width) {
+      x = canvas.width - logoWidth;
+    }
+    if (y + logoHeight > canvas.height) {
+      y = canvas.height - logoHeight;
+    }
+
+    // Update x and y speed
+    const speedScale = 0.002; // Adjust this value to control the speed scaling factor
+    const avgCanvasSize = (canvas.width + canvas.height) / 2;
+    const speed = avgCanvasSize * speedScale;
+    xSpeed = Math.sign(xSpeed) * speed;
+    ySpeed = Math.sign(ySpeed) * speed;
   }
-  if (y + logoHeight > canvas.height) {
-    y = canvas.height - logoHeight;
+
+  function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
   }
-
-  // Update x and y speed
-  const speedScale = 0.002; // Adjust this value to control the speed scaling factor
-  const avgCanvasSize = (canvas.width + canvas.height) / 2;
-  const speed = avgCanvasSize * speedScale;
-  xSpeed = Math.sign(xSpeed) * speed;
-  ySpeed = Math.sign(ySpeed) * speed;
-}
-
-function randomColor() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
-}
   function changeTintColor() {
     tintColor = randomColor();
   }
 
+  function updateCornerHitsLabel() {
+    const cornerHitsLabel = document.getElementById("cornerHits");
+    cornerHitsLabel.textContent = `Corner Hits: ${cornerHits}`;
+  }
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -67,6 +72,20 @@ function randomColor() {
     if (y < 0 || y > canvas.height - logoHeight) {
       ySpeed = -ySpeed;
       hitEdge = true;
+    }
+
+    if (x <= 0 && y <= 0) {
+      cornerHits++;
+      updateCornerHitsLabel();
+    } else if (x <= 0 && y >= canvas.height - logoHeight) {
+      cornerHits++;
+      updateCornerHitsLabel();
+    } else if (x >= canvas.width - logoWidth && y <= 0) {
+      cornerHits++;
+      updateCornerHitsLabel();
+    } else if (x >= canvas.width - logoWidth && y >= canvas.height - logoHeight) {
+      cornerHits++;
+      updateCornerHitsLabel();
     }
 
     if (hitEdge) {
